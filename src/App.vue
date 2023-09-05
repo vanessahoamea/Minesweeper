7df72cb3be92e2c52f8208413f172ba90e5ed917
Vanessa Hoamea <vanessahoamea@yahoo.com>
2023-09-05 12:36:09 +0300

    modal text and difficulty selection

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+    <div class="difficulty-buttons">
+        <button :class="{ selected: isEasy }" @click="changeDifficulty(DIFFICULTY.EASY)">easy</button>
+        <button :class="{ selected: isMedium }" @click="changeDifficulty(DIFFICULTY.MEDIUM)">medium</button>
+        <button :class="{ selected: isHard }" @click="changeDifficulty(DIFFICULTY.HARD)">hard</button>
+    </div>
+
     <Game :boardSize="boardSize" :mines="mines" />
 </template>
 
@@ -16,13 +22,48 @@ export default {
     components: { Game },
     data() {
         return {
+            DIFFICULTY,
             boardSize: DIFFICULTY.MEDIUM[0],
             mines: DIFFICULTY.MEDIUM[1]
         };
+    },
+    computed: {
+        isEasy() {
+            return this.boardSize === 8;
+        },
+        isMedium() {
+            return this.boardSize === 16;
+        },
+        isHard() {
+            return this.boardSize === 24;
+        },
+    },
+    methods: {
+        changeDifficulty(newDifficulty) {
+            [this.boardSize, this.mines] = newDifficulty;
+        }
     }
 };
 </script>
 
 <style>
+.difficulty-buttons {
+    display: flex;
+    justify-content: center;
+}
 
+.difficulty-buttons button {
+    margin: 5px;
+    padding: 5px;
+    border-radius: 10px;
+    border: 2px solid var(--mine-color);
+    background-color: var(--mine-background);
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.difficulty-buttons .selected {
+    background-color: var(--mine-color);
+}
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="backdrop" @click.self="closeModal">
         <div class="modal">
-            <p>Win: {{ modalData.win }}</p>
-            <p>Time: {{ modalData.seconds }}</p>
+            <h2>{{ title }}</h2>
+            <p>{{ message }}</p>
+            <p>Your time: {{ correctTime }} seconds</p>
         </div>
     </div>
 </template>
@@ -10,9 +11,32 @@
 <script>
 export default {
     props: ["modalData"],
+    data() {
+        return {
+            title: "",
+            message: ""
+        };
+    },
+    computed: {
+        correctTime() {
+            return this.modalData.seconds + 1;
+        }
+    },
     methods: {
         closeModal() {
             this.$emit("close");
+        }
+    },
+    mounted() {
+        if(this.modalData.win)
+        {
+            this.title = "You Win!";
+            this.message = "Congratulations! You cleared the game.";
+        }
+        else
+        {
+            this.title = "You Lose!";
+            this.message = "Better luck next time.";
         }
     }
 };
